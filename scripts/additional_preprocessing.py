238829dbf26ea33b6091a12b2c7a2cdc308a6130
Luis Duarte <luis.duarte.2003.13@hotmail.com>
2022-08-25 00:16:16 +0100

    Made the additional preprocessing better reacting to changes in component fields. It's still very hardcoded still needs more work

--- scripts/additional_preprocessing.py
+++ scripts/additional_preprocessing.py
@@ -1,3 +1,4 @@
+from __future__ import barry_as_FLUFL
 import glob
 import sys
 from io import SEEK_SET, TextIOWrapper
@@ -17,6 +18,9 @@ def component_class_rule(file: TextIOWrapper):
     found_component_class = False
     var_names = []
     skip = False
+    check_for_component_changes = False
+    function_var_names = {}
+    component_changes_line = -1
     for i,line in enumerate(lines):
         if "COMPONENT_CLASS_START" in line and not "#define" in line:
             found_component_class = True
@@ -25,13 +29,23 @@ def component_class_rule(file: TextIOWrapper):
                 #this means that a component field was outside a class which must not happen
                 print("Found a component outside a component class in file {}: {}".format(file.name,line),file=sys.stderr)
                 exit(1)
-            var_names.append(line.split('(')[1].split(',')[1].split(')')[0])
+            var_names.append(line.split('(')[1].split(',')[1].split(')')[0].replace(" ", ""))
         if "void addNewComponent()" in line and found_component_class==True:
-            #TODO: check for changes if class members change add/remove if needed
             skip = True
+            check_for_component_changes = True
+            function_var_names[i] = [] #reset function_var_names if there are multiple component classes just in case
+            component_changes_line = i
+            continue
+        if check_for_component_changes:
+            if line == "    }\n": 
+                check_for_component_changes=False
+                continue
+            #FIXME: this is not very dynamic if there are multiple functions, for now it's not needed
+            function_var_names[component_changes_line].append(line.split(".resize")[0].replace(" ",""))
+
+
         if "COMPONENT_CLASS_END" in line and not "#define" in line:
             if skip:
-                var_names = []
                 found_component_class = False
                 skip = False      
                 continue
@@ -46,12 +60,42 @@ def component_class_rule(file: TextIOWrapper):
             file.write("    }\n")
             file.writelines(lines[i:])
             component_class_rule(file)
-            var_names = []
             found_component_class = False
     if(found_component_class == True):
         print("Didn't close component class declaration in file: {}".format(file.name), file=sys.stderr)
         exit(1)
-        
+    if(component_changes_line == -1): return
+    #check for differences
+    #FIXME: this is not very dynamic if there are multiple functions, for now it's not needed
+    for line in list(function_var_names.keys()):
+        #if there are differences they must be or to add or to remove from the function
+        #this can be calculated by simple set math, because substraction is asymetrical in sets
+        differences_to_add = list(set(var_names) - set(function_var_names[line]))
+        if(len(differences_to_add) != 0):
+            file.truncate(0)
+            file.writelines(lines[:line+1])
+            for dif in differences_to_add:
+                file.write("        "+dif+".resize("+dif+".size()+1);\n")
+            file.writelines(lines[line+1:])
+            lines = file.readlines() #reload lines
+
+        #to remove
+        differences_to_remove = list(set(function_var_names[line]) - set(var_names))
+        if(len(differences_to_remove) != 0):
+            file.truncate(0)
+            file.writelines(lines[:line+1])
+            line_count = line+1
+            for l in lines[line+1:]:
+                if l == "    }\n": break
+                name = lines[line_count].split(".resize")[0].replace(" ","")
+                if name in differences_to_remove:
+                    line_count += 1
+                    continue
+                file.write(lines[line_count])
+                line_count += 1
+
+            file.writelines(lines[line_count:])
+
 
 
 
